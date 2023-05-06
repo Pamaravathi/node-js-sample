8265bdbd55efc5b2d3649f89e4ef0010aae34edf
Pamaravathi <132546620+Pamaravathi@users.noreply.github.com>
2023-05-06 18:15:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello guys this ejaz from devops team..')
+  response.send('application deploy..')
 })
 
 app.listen(app.get('port'), function() {
